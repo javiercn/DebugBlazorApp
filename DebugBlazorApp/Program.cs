ebd72f7a6f118323605cf3a9c2f7ca9a4f94ca13
jacalvar <jacalvar@microsoft.com>
2023-12-12 15:53:28 +0100

    Add project files.

---
+++ DebugBlazorApp/Program.cs
@@ -0,0 +1,36 @@
+using DebugBlazorApp.Client.Pages;
+using DebugBlazorApp.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<LazyAssemblyLoader>();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
